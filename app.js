7028c8600b82456e8c1f45b32cdaee9c124828dd
bilawal shaikh <bilawal.shaikh027@gmail.com>
2020-12-02 16:16:08 +0500

    test

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
-// var btnClick=document.querySelector("#btn-click");
-// var txtArea=document.querySelector(".txt-area");
-// var transTxt=document.querySelector(".trans-txt");
+var btnClick=document.querySelector("#btn-click");
+var txtArea=document.querySelector("#txt-area");
+var transTxt=document.querySelector("#trans-txt");
 
 
-
-
-// var serverURL="https://api.funtranslations.com/translate/minion.json"
-
-
-
-// function getTranslationURL(text){
- 
-//     return  serverURL +  "?"+ "text" + text
-// }
-
-// function errorHandler(error) {
-//     console.log("error occured", error);
-//     alert("something wrong with server! try again after some time")
-// }
-
-// function clickme(){
-//     var textInput=txtArea.value;
-
-//     fetch(getTranslationURL(textInput))
-//        .then(response => response.json())
-//     .then(json =>  {
-//         var translatedText = json.Contents.translated;
-//     transTxt.innerText = translatedText; // output
-//    })
-//    .catch(errorHandler)
-  
-// }
-// btnClick.addEventListener("click",clickme)
-
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
 
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
@@ -53,7 +20,8 @@ function errorHandler(error) {
 
 
 function clickMe() {
-    var textInput = txtInput.value; // taking input
+    
+    var textInput = txtArea.value; // taking input
 
 
 
@@ -61,9 +29,9 @@ function clickMe() {
        .then(response => response.json())
        .then(json =>  {
         var translatedText =json.contents.translated;
-     outputDiv.innerText =translatedText;})
+     transTxt.innerText =translatedText;})
    
 
 }
 
-btnTranslate.addEventListener("click", clickMe)
+btnClick.addEventListener("click", clickMe)
